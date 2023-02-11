39110b66b0b60f1addd6cfb477fe674ceccba5a3
kikusko77 <112938118+kikusko77@users.noreply.github.com>
2023-02-11 17:54:05 +0100

    Add files via upload

---
+++ excel/stanoapp.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+import pandas as pd
+from tkinter import filedialog, messagebox
+
+def scrape_data():
+    # Open a file dialog to select the first Excel file
+    file_path1 = filedialog.askopenfilename(filetypes=[("Excel Files", ".xlsx;.xlsb")])
+    print(file_path1)
+    # Read the first Excel file
+    df1 = pd.read_excel(file_path1, engine='pyxlsb')
+
+    # Open a file dialog to select the second Excel file
+    file_path2 = filedialog.askopenfilename(filetypes=[("Excel Files", ".xlsx;.xlsb")])
+
+    # Read the second Excel file
+    df2 = pd.read_excel(file_path2, engine='pyxlsb')
+
+    # Get the list of columns from the second Excel file
+    columns = list(df2.columns)
+
+    # Ask the user to choose the column with conditions
+    column_name = tk.simpledialog.askstring("Column Name", "Enter the name of the column with conditions:", parent=root, initialvalue=columns[0])
+
+    # Check if the user entered a valid column name
+    if column_name not in columns:
+        messagebox.showerror("Error", "The entered column name is invalid. Please enter a valid column name.")
+        return
+
+    # Get the conditions from the first Excel file
+    conditions = df1['Export'].tolist()
+
+    # Filter the data in the second Excel file based on the conditions
+    df2 = df2[df2[column_name].isin(conditions)]
+    df2 = df2[df2[column_name].str.contains("->") & ~df2[column_name].str.contains("vyrazen")]
+
+    # Get the filtered data
+    filtered_data = []
+    for i, row in df2.iterrows():
+        condition = row[column_name]
+        course_sequence = condition.split("->")[-1].strip()
+        if "Vyrazen" not in course_sequence:
+            filtered_data.append(row.to_dict())
+
+    # Open a file dialog to save the filtered data as an Excel file
+    file_path3 = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel Files", "*.xlsx")])
+
+    # Save the filtered data as an Excel file
+    pd.DataFrame(filtered_data).to_excel(file_path3, index=False)
+
+# Create the main window
+root = tk.Tk()
+root.title("Excel Scraper")
+root.geometry("600x400")
+
+# Add a button to the window
+button = tk.Button(root, text="Scrape excel", command=scrape_data, font=("Helvetica", 20), height=2, width=20)
+button.pack(pady=100)
+
+# Center the button
+root.update_idletasks()
+w = button.winfo_width()
+h = button.winfo_height()
+x = (root.winfo_width() // 2) - (w // 2)
+y = (root.winfo_height() // 2) - (h // 2)
+button.place(x=x, y=y)
+
+# Start the main event loop
+root.mainloop()
+
